a9078fc030b92f82447bdc0bcc979ebdf9f2c64f
= <=>
2018-02-20 19:12:07 +0900

    ini

--- nazotoki/FirstViewController.swift
+++ nazotoki/FirstViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
 
+
+//プロトコルの作成
+protocol FirstviewDelegate {
+    //委託したいのはviewを追加すること
+    func addview()
+}
+
+
+class FirstViewController: UIViewController {
+    var delegate:FirstviewDelegate? = nil
+    let buttun = UIButton()
+    let secondviewinstance = SecondViewController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    // ボタンの設定
+        buttun.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        buttun.backgroundColor = UIColor.blue
+        buttun.setTitle("ボタン", for: .normal)
+        buttun.addTarget(self, action: #selector(FirstViewController.tapbottun(sender:)), for: .touchUpInside)
+        self.view.addSubview(buttun)
     }
+    
+    @objc func tapbottun(sender: Any) { // buttonの色を変化させるメソッド
+        if let dg = self.delegate {
+            dg.addview()
+            self.delegate = secondviewinstance
+        }
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- nazotoki/SecondViewController.swift
+++ nazotoki/SecondViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,FirstviewDelegate {
 
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    func addview() {
+        let Rect = CGRect(x: 0, y: 0, width: 200, height: 200)
+        let imageview = UIImageView(frame: Rect)
+        imageview.contentMode = .scaleAspectFit
+        imageview.image = UIImage(named: "Image")
+        imageview.center = self.view.center
+        self.view.addSubview(imageview)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
