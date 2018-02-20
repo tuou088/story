3618e76b2b69901271ddbc1d574ff644412e8398
= <=>
2018-02-20 15:25:27 +0900

    Initial Commit

---
+++ nazotokiTests/nazotokiTests.swift
@@ -0,0 +1,36 @@
+//
+//  nazotokiTests.swift
+//  nazotokiTests
+//
+//  Created by hidkei haruta on 2018/02/20.
+//  Copyright © 2018年 hidkei haruta. All rights reserved.
+//
+
+import XCTest
+@testable import nazotoki
+
+class nazotokiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
